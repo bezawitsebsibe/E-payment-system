82a26981c702e73546f67434dcaf7db366e66ea0
beza_sebsibe <88824956+bezawitsebsibe@users.noreply.github.com>
2023-09-19 12:38:36 +0100

    Update userRegistration.js

--- front-end/src/pages/userRegistration.js
+++ front-end/src/pages/userRegistration.js
@@ -1,79 +1,83 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, Switch } from 'react-router-dom';
 import axios from 'axios';
-import './homePage.css';
-import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import companyLogo from '../image/logoimage.jpg';
+import { useNavigate } from 'react-router-dom';
 
 const RegistrationForm = () => {
+  
   const [formData, setFormData] = useState({
     FirstName: '',
     LastName: '',
     Gender: '',
     UserName: '',
     Password: '',
-    ConfirmPassword:'',
-    Email:'',
+    ConfirmPassword: '',
+    Email: '',
     PhoneNumber: '+251',
-    Address:'',
+    Address: '',
   });
 
   const [errors, setErrors] = useState({});
+  const [selectedGender, setSelectedGender] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const validateForm = () => {
     const newErrors = {};
 
+
     if (!formData.FirstName) {
-      newErrors.FirstName = 'first is required';
+      newErrors.FirstName = 'First Name is required';
+    } else if (!/^[A-Za-z]+$/.test(formData.FirstName)) {
+      newErrors.FirstName = 'First Name should only contain letters';
     }
-
+     
     if (!formData.LastName) {
-      newErrors.LastName = 'LastName is required';
+      newErrors.LastName = 'Last Name is required';
+    } else if (!/^[A-Za-z]+$/.test(formData.LasttName)) {
+      newErrors.LastName = 'Last Name should only contain letters';
     }
 
-    if (!formData.Gender) {
+    if (!selectedGender) {
       newErrors.Gender = 'Gender is required';
     }
-
     if (!formData.UserName) {
-      newErrors.UserName = 'username is required';
+      newErrors.UserName = 'User name is required';
     }
 
     if (!formData.Password) {
-      newErrors.Password = 'password is required';
+      newErrors.Password = 'Password is required';
     }
-    if (!formData.ConfirmPassword ) {
-      newErrors.ConfirmPassword  = 'password is required';
-    }else if (formData.Password !== formData.ConfirmPassword) {
+
+    if (!formData.ConfirmPassword) {
+      newErrors.ConfirmPassword = 'Confirm password is required';
+    } else if (formData.Password !== formData.ConfirmPassword) {
       newErrors.ConfirmPassword = 'Passwords do not match';
     }
 
     if (!formData.Email) {
-        newErrors.Email = 'email is required';
-      }
-      else if (!/\S+@\S+\.\S+/.test(formData.Email)) {
-        newErrors.Email = "Email is invalid";
-      }
-      if (!formData.Address) {
-        newErrors.Address = 'address is required';
-      }
-
-
-    if (!formData.PhoneNumber) {
-      newErrors.PhoneNumber = "Phone Number is required";
-    } else if (!/^\+[0-9\s-()]+$/.test(formData.PhoneNumber)) {
-      newErrors.PhoneNumber = "Phone Number is invalid";
-    } else if (formData.PhoneNumber.replace(/[^\d]/g, "").length < 12) {
-      newErrors.PhoneNumber = "Phone Number must have at least 12 digits";
+      newErrors.Email = 'Email is required';
+    } else if (!/\S+@\S+\.\S+/.test(formData.Email)) {
+      newErrors.Email = 'Email is invalid';
     }
 
-  
+    if (!formData.Address) {
+      newErrors.Address = 'Address is required';
+    }
+
+    if (!formData.PhoneNumber) {
+      newErrors.PhoneNumber = 'Phone number is required';
+      
+    } else if (!/^\+[0-9\s-()]+$/.test(formData.PhoneNumber)) {
+      newErrors.PhoneNumber = 'Phone number is invalid';
+    }
 
     setErrors(newErrors);
     return Object.keys(newErrors).length === 0; // Return true if there are no errors
   };
 
- 
   const checkUserExists = async (email) => {
     try {
       const response = await axios.get('http://localhost:3001/Users');
@@ -88,7 +92,8 @@ const RegistrationForm = () => {
   const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (validateForm()) {
+    if (validateForm()&& selectedGender) {
+      setIsLoading(true);
       try {
         const userExists = await checkUserExists(formData.Email);
         if (userExists) {
@@ -98,7 +103,7 @@ const RegistrationForm = () => {
           const formDataToSend = new FormData();
           formDataToSend.append('FirstName', formData.FirstName);
           formDataToSend.append('LastName', formData.LastName);
-          formDataToSend.append('Gender', formData.Gender);
+          formDataToSend.append('Gender', selectedGender);
           formDataToSend.append('UserName', formData.UserName);
           formDataToSend.append('Password', formData.Password);
           formDataToSend.append('PhoneNumber', formData.PhoneNumber);
@@ -107,15 +112,28 @@ const RegistrationForm = () => {
   
           await axios.post('http://localhost:3001/Users', formDataToSend);
   
-          console.log('Registered successfully!');
+         
           // Perform navigation to the service providers
           navigate('/signup/serviceProviders');
-        }
+        // Display success message using Toastify
+        toast.success('Registered successfully!', {
+          position: toast.POSITION.TOP_RIGHT,
+          autoClose: 3000,
+        });
+console.log('registration successful');
+        setIsSubmitted(true);
+      }
       } catch (error) {
         console.error('Error submitting form:', error);
+      } finally {
+        setIsLoading(false);
       }
     }
   };
+  const handleGenderChange = (e) => {
+    setSelectedGender(e.target.value);
+  };
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
@@ -125,7 +143,6 @@ const RegistrationForm = () => {
   };
   return (
     <div className='container'>
-
     <div className='header'>
       <div className='logo'>
         <img src={companyLogo} alt='company logo' />
@@ -135,131 +152,143 @@ const RegistrationForm = () => {
         </div>
       </div>
 </div>
-<div className='body'>
-    <form onSubmit={handleSubmit}>
-      <div className="form-box">
-      
-        <div>
-          <label>FirstName</label>
+    
+<div className='user-body'>
+  <ToastContainer />
+
+  <form onSubmit={handleSubmit}>
+    <div className="form">
+      <div className='title'>Register</div>
+      <div className='user-detail'>
+        <div className='item'>
+          <label className='user-input'>First Name:</label>
           <input
             type="text"
             name="FirstName"
-            className="input-field"
             value={formData.FirstName}
             onChange={handleChange}
-            placeholder="enter firstname "
+            placeholder="Enter first name"
           />
           {errors.FirstName && <span>{errors.FirstName}</span>}
         </div>
-        <div>
-          <label>LastName:</label>
+        <div className='item'>
+          <label className='user-input'>Last Name:</label>
           <input
             type="text"
             name="LastName"
-            className="input-field"
             value={formData.LastName}
             onChange={handleChange}
-            placeholder="enter lastname "
+            placeholder="Enter last name"
           />
           {errors.LastName && <span>{errors.LastName}</span>}
         </div>
-        <div>
-          <label>Gender:</label>
-          <input
-            type="text"
-            name="Gender"
-            className="input-field"
-            value={formData.Gender}
-            onChange={handleChange}
-            placeholder="enter gender "
-          />
-          {errors.Gender && <span>{errors.Gender}</span>}
-        </div>
-        <div>
-          <label>UserName:</label>
+        <div className='item'>
+          <label className='user-input'>User Name:</label>
           <input
             type="text"
             name="UserName"
-            className="input-field"
             value={formData.UserName}
             onChange={handleChange}
-            placeholder="enter username"
+            placeholder="Enter user name"
           />
-          {errors.UserName && <span >{errors.UserName}</span>}
+          {errors.UserName && <span>{errors.UserName}</span>}
         </div>
-        <div>
-          <label>Password:</label>
+        <div className='item'>
+          <label className='user-input'>Password:</label>
           <input
-            type="text"
+            type="password"
             name="Password"
-            className="input-field"
             value={formData.Password}
             onChange={handleChange}
-            placeholder="enter password "
+            placeholder="Enter password"
           />
           {errors.Password && <span>{errors.Password}</span>}
         </div>
-        <div>
-          <label>Confirm password</label>
+        <div className='item'>
+          <label className='user-input'>Confirm Password:</label>
           <input
-            type="text"
-            name=" ConfirmPassword"
-            className="input-field"
+            type="password"
+            name="ConfirmPassword"
             value={formData.ConfirmPassword}
             onChange={handleChange}
-            placeholder="Re-enter password "
+            placeholder="Confirm password"
           />
           {errors.ConfirmPassword && <span>{errors.ConfirmPassword}</span>}
         </div>
-        <div>
-          <label>Phone Number:</label>
+        <div className='item'>
+          <label className='user-input'>Email:</label>
           <input
-            type="text"
-            name="PhoneNumber"
-            className="input-field"
-            value={formData.PhoneNumber}
-            onChange={handleChange}
-            placeholder="enter phone number"
-          />
-          {errors.PhoneNumber && <span>{errors.PhoneNumber}</span>}
-        </div>
-        <div>
-          <label>Email:</label>
-          <input
-            type="text"
+            type="email"
             name="Email"
-            className="input-field"
             value={formData.Email}
             onChange={handleChange}
-            placeholder="enter email address"
+            placeholder="Enter email"
           />
           {errors.Email && <span>{errors.Email}</span>}
         </div>
-       
-        <div>
-          <label>Address:</label>
+        <div className='item'>
+          <label className='user-input'>Phone Number:</label>
+          <input
+            type="tel"
+            name="PhoneNumber"
+            value={formData.PhoneNumber}
+            onChange={handleChange}
+            placeholder="Enter phone number"
+          />
+          {errors.PhoneNumber && <span>{errors.PhoneNumber}</span>}
+        </div>
+        <div className='item'>
+          <label className='user-input'>Address:</label>
           <input
             type="text"
             name="Address"
-            className="input-field"
             value={formData.Address}
             onChange={handleChange}
-            placeholder="enter address "
+            placeholder="Enter address"
           />
           {errors.Address && <span>{errors.Address}</span>}
         </div>
-        <br></br>
-      
-        <button className="button" type="button" htmlType="submit" >
-           Submit
-          </button>
-        
+        <div className='item'>
+          <label className='user-input'>Gender:</label>
+          <div className='category'>
+            <label htmlFor='dot-1'>
+              <span className='dot one'></span>
+              <span className='gender'>Male</span>
+              <input
+                   type='radio'
+                  name='gender'
+                   id='dot-1'
+                   value='male'
+                checked={selectedGender === 'male'}
+                onChange={handleGenderChange}
+               />
+            </label>
+            <label htmlFor='dot-2'>
+              <span className='dot two'></span>
+              <span className='gender'>Female</span>
+              <input
+                  type='radio'
+                  name='gender'
+                  id='dot-2'
+                  value='female'
+                  checked={selectedGender === 'female'}
+                  onChange={handleGenderChange}
+               />
+            </label>
+          </div>
+          {errors.Gender && <span className="error">{errors.Gender}</span>}
+        </div>
+      </div>
+      <button className="button" type="submit" disabled={isLoading}>
+        {isLoading ? 'Submitting..' : 'Submit'}
+      </button>
     </div>
   </form>
-  </div>
-</div>
-);
-};
+  {isSubmitted && <div>Registration successful!</div>}
 
+    </div>
+    </div>
+  );
+};
 
 export default RegistrationForm;
