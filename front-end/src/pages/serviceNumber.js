2c2a5ce312ef40ba12d82ef7d1a843e3afc47691
beza_sebsibe <88824956+bezawitsebsibe@users.noreply.github.com>
2023-10-02 12:46:23 +0100

    Update serviceNumber.js

--- front-end/src/pages/serviceNumber.js
+++ front-end/src/pages/serviceNumber.js
@@ -1,23 +1,68 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { message } from 'antd';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import './homePage.css';
 import axios from 'axios';
 import companyLogo from '../image/logoimage.jpg';
 
 const ServiceNumber = () => {
-  const [paymentFor, setPaymentFor] = useState('');
   const [serviceNumber, setServiceNumber] = useState('');
+  const [serviceProviderBIN, setServiceProviderBIN] = useState(localStorage.getItem('serviceProviderBIN'));
+  const [bill, setBill] = useState(null);
+  const [user, setUser] = useState(null);
+  const [data, setData] = useState({});
+  const [paymentFor, setPaymentFor] = useState('');
   const [errors, setErrorMessage] = useState('');
+  const navigate = useNavigate();
 
-  const handlePaymentForChange = (event) => {
-    setPaymentFor(event.target.value);
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Retrieve user data from localStorage or API
+        const userData = localStorage.getItem('userData');
+        const parsedUserData = JSON.parse(userData);
+        const user = parsedUserData?.user || {};
+        setUser(user);
+        console.log(user);
+
+        // Fetch the user data including service providers
+        const response = await axios.get(`http://localhost:3001/Users/${user.id}`);
+        const { data } = response;
+        console.log(data);
+
+        // Filter service providers based on serviceProviderBIN
+        const filteredServiceProviders = data.ServiceProviders.filter(
+          (provider) => provider.serviceProviderBIN === serviceProviderBIN
+        );
+
+        if (filteredServiceProviders.length > 0) {
+          const serviceNos = filteredServiceProviders.map(
+            (provider) => provider.userServiceProvider.serviceNo
+          );
+          setServiceNumber(serviceNos.join(', '));
+        }
+
+        console.log(filteredServiceProviders);
+        console.log(serviceNumber);
+        setData(data);
+      } catch (error) {
+        console.error('Error fetching service number:', error);
+        message.error('Failed to fetch service number');
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const handleServiceNumberChange = (event) => {
     setServiceNumber(event.target.value);
   };
 
-  const handlePaymentSubmit = (event) => {
+  const handlePaymentForChange = (event) => {
+    setPaymentFor(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
     event.preventDefault();
 
     if (!serviceNumber) {
@@ -25,79 +70,87 @@ const ServiceNumber = () => {
       return;
     }
 
-    // Process payment based on paymentFor and serviceNumber values
-    console.log('Payment processed');
-    console.log('Payment For:', paymentFor);
-    console.log('Service Number:', serviceNumber);
-
-    // Reset form
-    setPaymentFor('');
-    setServiceNumber('');
-    setErrorMessage('');
-
-    // Navigate to '/payment'
-    navigate('/payment');
+    localStorage.setItem('serviceNo', serviceNumber);
+    console.log(localStorage.getItem('serviceNo'));
+    navigate('/payment', { state: { serviceNumber } });
   };
 
-  const navigate = useNavigate();
-
   return (
-    <div className="container">
-      <div className="header">
-        <div className="logo">
-          <img src={companyLogo} alt="company logo" />
-          <div className="company-name">
+    <div className='container'>
+      <div className='header'>
+        <div className='logo'>
+          <img src={companyLogo} alt='company logo' />
+          <div className='company-name'>
             E-payment-system
-            <div className="slogan">your trusted online payment system</div>
+            <div className='slogan'>your trusted online payment system</div>
           </div>
         </div>
       </div>
-      <div className="bodyy">
-        <div className="form-container">
-          <form onSubmit={handlePaymentSubmit}>
-            <div>
-              <label>
-                <input
-                  type="radio"
-                  value="self"
-                  checked={paymentFor === 'self'}
-                  onChange={handlePaymentForChange}
-                />
-                Make payment for myself
-              </label>
-              <label>
-                <input
-                  type="radio"
-                  value="other"
-                  checked={paymentFor === 'other'}
-                  onChange={handlePaymentForChange}
-                />
-                Make payment for someone else
-              </label>
-            </div>
-
-            {paymentFor && (
+      <div className='circle'>
+        <p>P</p>
+      </div>
+      
+        <div className='bodyy'>
+          <div className='form-container'>
+            <form onSubmit={handleSubmit}>
+            <div className='title'>Payment Page</div>
               <div>
                 <label>
-                  Please enter the service number:
                   <input
-                    type="text"
-                    value={serviceNumber}
-                    onChange={handleServiceNumberChange}
+                    type='radio'
+                    value='self'
+                    checked={paymentFor === 'self'}
+                    onChange={handlePaymentForChange}
                   />
+                  Make payment for myself
+                </label>
+                <label>
+                  <input
+                    type='radio'
+                    value='other'
+                    checked={paymentFor === 'other'}
+                    onChange={handlePaymentForChange}
+                  />
+                  Make payment for someone else
                 </label>
               </div>
-            )}
+              {paymentFor === 'self' && user && (
+                <>
+                  <p>User ID: {user.UserID}</p>
+                  <p>First Name: {user.FirstName}</p>
+                  <p>Last Name: {user.LastName}</p>
+                  {/* Display other user details */}
+                  <label>
+                    <br></br>
+                    Service Number:
+                    <input
+                      type='text'
+                      value={serviceNumber}
+                      onChange={handleServiceNumberChange}
+                    />
+                  </label>
+                  <button type='submit'>NEXT</button>
+                </>
+              )}
+              {paymentFor === 'other' && (
+                <>
+                  <label>
+                    Service Number:
+                    <input
+                      type='text'
+                      onChange={handleServiceNumberChange}
+                    />
+                  </label>
+                  <button type='submit'>NEXT</button>
+                </>
+              )}
 
-            {errors && <span> {errors}</span>}
-        
-            <button type="submit" disabled={!paymentFor || !serviceNumber}>
-              Next
-            </button>
-          </form>
+              {errors && <span>{errors}</span>}
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+   
   );
 };
 
