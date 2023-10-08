ea7bd34d4895bb4de31deae11ebf8f34e3cd8597
beza_sebsibe <88824956+bezawitsebsibe@users.noreply.github.com>
2023-10-08 19:55:59 +0100

    Update payment.js

--- front-end/src/pages/payment.js
+++ front-end/src/pages/payment.js
@@ -1,124 +1,327 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import { Button, Form, Input, Modal, message } from "antd";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 import axios from "axios";
-import { Button, Input } from "antd";
-import "./payment.css";
+import "./style.css";
+import { MailOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
+
 
 const Payment = () => {
+  const [userData, setUserData] = useState(localStorage.getItem('userData'));
+  const [serviceNo, setServiceNumber] = useState(localStorage.getItem('serviceNo'));
+  const [user, setUser] = useState(null);
   const [payments, setPayments] = useState([]);
+  const [userbill, setUserBill] = useState(null);
+  const [banks, setBanks] = useState([]);
+  const [form] = Form.useForm();
+  const [userId, setUserId] = useState();
+  const [downloadModalVisible, setDownloadModalVisible] = useState(false);
+  const formRef = useRef(null);
+  const [verificationCode, setVerificationCode] = useState('');
+  const [verifyCode, setVerifyCode] = useState('');
+  const [errors, setErrorMessage] = useState('');
+  const [showBankAccountForm, setShowBankAccountForm] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+
   const [bankAccount, setBankAccount] = useState({
     bankAccountNumber: "",
+    bankName: "",
     account_holder_name: "",
     account_holder_type: "individual",
   });
 
-  const [showBankAccountForm, setShowBankAccountForm] = useState(false);
 
+  useEffect(()=>{
+      localStorage.setItem("userSelectedMenu", 4);
+    },[])
+    
   useEffect(() => {
-    fetchPayments();
-  }, []);
+    if (!userData) {
+      navigate("/users");
+      return;
+    }
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3001/Users/serviceNo/${serviceNo}`);
+        setUser(response.data);
+        console.log(response.data);
 
-  const fetchPayments = async () => {
-    try {
-      const response = await axios.get("http://localhost:3001/bills");
-      setPayments(response.data);
-    } catch (error) {
-      console.error(error);
-    }     
-  };
+        // Assuming user has a valid service provider association
+        const serviceProviderBIN = response.data.ServiceProviders[0].serviceProviderBIN;
+        const userId = response.data.id;
+        setUserId(userId);
+        console.log("serviceProviderBIN: ", serviceProviderBIN);
+
+        const userBillResponse = await axios.get(`http://localhost:3001/bills/findOne`, {
+          params: {
+            userId: userId,
+            serviceProviderBIN: serviceProviderBIN,
+          }
+        });
+        setUserBill(userBillResponse.data);
+        console.log(verificationCode);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    
+
+    const fetchBanks = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/Agents"); // Replace with your actual endpoint
+        setBanks(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+    fetchBanks();
+  }, []);
 
   const handlePayment = (billNumber, serviceProviderBIN) => {
     setShowBankAccountForm(true);
   };
 
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setBankAccount((prevBankAccount) => ({
+      ...prevBankAccount,
+      [name]: value,
+    }));
+  };
+
+
+  const sendVerificationEmail = async () => {
+    try {
+      setLoading(true);
+      // Generate a verification code
+      const code = generateVerificationCode();
+      setVerificationCode(code);
+
+      // Send the verification code to the fetched email address
+      console.log('userId:', userId)
+      const response = await axios.post(`http://localhost:3001/Users/verifyUser/${userId}/${code}`);
+
+
+      console.log('Email sent successfully');
+      message.success('Email sent successfully');
+      setLoading(false);
+      // email is successfully sent
+    } catch (error) {
+      console.error('Error sending email:', error);
+      message.error('Error sending email:', error);
+    }
+  };
+
+  const handleDownload = (fileType) => {
+    // Capture the modal container element
+    const modalContainer = document.querySelector(".ant-modal-content");
+
+    // Exclude the footer element from the captured canvas
+    const footer = modalContainer.querySelector(".footer");
+    const buttons = modalContainer.querySelectorAll(".ant-btn");
+
+    // Hide the buttons temporarily
+    buttons.forEach((button) => {
+      button.style.display = "none";
+    });
+
+    if (footer) {
+      footer.style.display = "none";
+    }
+
+    // Use html2canvas to convert the modal container to a canvas
+    html2canvas(modalContainer).then((canvas) => {
+      // Restore the display of the buttons and footer elements
+      buttons.forEach((button) => {
+        button.style.display = "block";
+      });
+
+      if (footer) {
+        footer.style.display = "block";
+      }
+
+      if (fileType === "picture") {
+        // Convert the canvas to a base64-encoded PNG image
+        const dataURL = canvas.toDataURL("image/png");
+
+        // Create a temporary link element to trigger the download
+        const link = document.createElement("a");
+        link.href = dataURL;
+        link.download = "modal.png";
+        link.click();
+      } else if (fileType === "pdf") {
+        // Convert the canvas to a base64-encoded PNG image
+        const dataURL = canvas.toDataURL("image/png");
+
+        // Calculate the dimensions of the PDF document based on the canvas size
+        const imgWidth = 210; // A4 page width (in mm)
+        const imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+        // Create a new jsPDF instance
+        const pdf = new jsPDF("p", "mm", "a4");
+
+        // Add the image to the PDF document
+        pdf.addImage(dataURL, "PNG", 0, 0, imgWidth, imgHeight);
+
+        // Save the PDF file
+        pdf.save("modal.pdf");
+      } else {
+        console.error("Invalid file type");
+      }
+    });
+  };
+
   const handleBankAccountSubmit = async () => {
     try {
+      let errorMessage = {};
+      if (!verifyCode || !bankAccount.bankAccountNumber || !bankAccount.AgentName) {
+
+
+        if (!bankAccount.bankAccountNumber || !bankAccount.AgentName) {
+          errorMessage.bankAccount = "Bank Account Number and Bank selection are required";
+        }
+        if (!verifyCode) {
+          errorMessage.verificationCode = "Verification code is required";
+        }
+
+        setErrorMessage(errorMessage);
+        return;
+      }
+
+      else if (verifyCode !== verificationCode) {
+        errorMessage.verificationCode = "Invallid Verification code";
+        setErrorMessage(errorMessage);
+        return;
+      }
+
+
+      const randomNumber = Math.floor(Math.random() * 1000000000);
+      const random = `TXN${randomNumber}`;
+      const today = new Date().toISOString().split('T')[0];
+
+      console.log(userbill);
+
+      const paymentData = {
+        TransactionNo: random,
+        paymentDate: today,
+        amount: userbill.TotalAmount,
+        UserId: userbill.UserId,
+        serviceProviderBIN: localStorage.getItem('serviceProviderBIN'),
+        paymentMethod: "Credit card",
+        paymentDescription: `Payment for ${userbill.serviceDescription} services`,
+        ReferenceNo: userbill.billNumber,
+      };
+
       // Create and verify the PaymentMethod
       const paymentMethodResponse = await axios.post(
-        "http://localhost:3001/create-payment-method",
-        {
-          bankAccountNumber: bankAccount.bankAccountNumber,
-          bankAccountName: bankAccount.account_holder_name,
-        }
+        "http://localhost:3001/payment",
+        paymentData
       );
 
-      const paymentMethodId = paymentMethodResponse.data.paymentMethodId;
-
-      // Find the selected bill from the payments array
-    const selectedBill = payments.find((bill) => bill.selected);
-
-    if (selectedBill) {
-      const response = await axios.post('http://localhost:3001/payment', {
-        billNumber: selectedBill.billNumber,
-        serviceProviderBIN: selectedBill.serviceProviderBIN,
-        amount: selectedBill.totalAmount,
-        currency: 'ETB', // Replace with the actual currency
-        bankAccount,
-        paymentMethodId,
+      // Update the bill status to "paid"
+      const response = await axios.put(`http://localhost:3001/bills/${userbill.id}`, {
+        billStatus: "paid",
+        PaymentId: paymentMethodResponse.data.id,
       });
-    
 
-      if (response.data.status === "succeeded") {
+      if (response) {
         // Payment succeeded
         console.log("Paid");
+        message.success('Payment successful');
+        setErrorMessage("");
+        setDownloadModalVisible(true);
+        setPayments(paymentData);
+        console.log(payments);
+
+        const paymentHistory = JSON.parse(localStorage.getItem("paymentHistorys")) || [];
+        paymentHistory.push(paymentData);
+        localStorage.setItem("paymentHistorys", JSON.stringify(paymentHistory));
+        console.log(localStorage.getItem('paymentHistory'))
+
       } else {
         // Payment failed
         console.log("Payment failed");
       }
-    }
     } catch (error) {
       console.error(error);
     }
-  
+
+  };
+
+  const generateVerificationCode = () => {
+    const length = 6; // Length of the verification code
+    const characters = "0123456789"; // Characters to use for the code
+    let code = "";
+
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      code += characters.charAt(randomIndex);
+    }
+
+    return code;
   };
 
 
-  const handleChange = (e) => {
-    setBankAccount({
-      ...bankAccount,
-      [e.target.name]: e.target.value,
-    });
+  const handleVerificationCodeChange = (event) => {
+    setVerifyCode(event.target.value);
+  };
+  const navigateToUserPage = () => {
+    navigate('/serviceProviders');
   };
 
   return (
     <div className="payment-container">
       <h1>Bill Detail</h1>
-      
-      {payments.map((bill) => (
-        <div key={bill.billNumber} className="bill-container">
+
+      {userbill ? (
+        <div key={userbill.billNumber} className="bill-container">
           <div className="bill-header">
-            <h2>Bill Number: {bill.billNumber}</h2>
-            <h3>Date Issued: {bill.dateIssued}</h3>
+            <h2>Bill Number: {userbill.billNumber}</h2>
+            <h3>Date Issued: {userbill.dateIssued}</h3>
           </div>
           <div className="bill-details">
             <div className="bill-section">
               <h4>Customer Information</h4>
-              <p>Customer Name: {bill.customerName}</p>
+              <p>Customer Name: {userbill.customerName}</p>
             </div>
             <div className="bill-section">
               <h4>Service Details</h4>
-              <p>Description: {bill.serviceDescription}</p>
-              <p>Period: {bill.servicePeriod}</p>
-              <p>Charges: {bill.serviceCharges}</p>
-              <p>Additional Charges: {bill.additionalCharges}</p>
+              <p>Description: {userbill.serviceDescription}</p>
+              <p>Period: {userbill.servicePeriod}</p>
+              <p>Service Charges: {userbill.serviceCharges}</p>
+              <p>Additional Charges: {userbill.additionalCharges}</p>
             </div>
             <div className="bill-section">
               <h4>Payment Details</h4>
-              <p>Amount Due: {bill.amountDue}</p>
-              <p>Due Date: {bill.dueDate}</p>
-              <p>Bill Status: {bill.billStatus}</p>
-              <p>Total Amount: {bill.totalAmount}</p>
+              <p>Amount Due: {userbill.amountDue}</p>
+              <p>Due Date: {userbill.dueDate}</p>
+              <p>Bill Status: {userbill.billStatus}</p>
+              <p>Total Amount: {userbill.TotalAmount}</p>
             </div>
           </div>
           <Button
             className="pay-btn"
             onClick={() =>
-              handlePayment(bill.billNumber, bill.serviceProviderBIN)
+              handlePayment(userbill.billNumber, userbill.serviceProviderBIN)
             }
           >
             Pay Now
           </Button>
         </div>
-      ))}
+      ) : (
+        <div className="no-bill">
+          <h2>No bill available.</h2>
+        </div>
+      )}
+
       {showBankAccountForm && (
         <div className="input-container">
           <h2>Bank Account Details</h2>
@@ -129,17 +332,94 @@ const Payment = () => {
             onChange={handleChange}
             placeholder="Bank Account Number"
           />
-          <Input
-            className="shorter-input"
-            name="account_holder_name"
-            value={bankAccount.account_holder_name}
+          <select
+            className="bank-dropdown"
+            name="AgentName"
+            value={bankAccount.AgentName}
             onChange={handleChange}
-            placeholder="Account Holder Name"
+          >
+            <option value="">Select Bank</option>
+            {banks.map((bank) => (
+              <option key={bank.id} value={bank.agentName}>
+                {bank.agentName}
+              </option>
+            ))}
+          </select>
+<br />
+          {errors.bankAccount && <span className="error-message" >{errors.bankAccount}</span>}
+
+          <Input
+            className="verification-input"
+            value={verifyCode}
+            onChange={handleVerificationCodeChange}
+            placeholder="Verification Code"
           />
+          <Button
+            className="verify-email-btn"
+            icon={<MailOutlined />}
+            onClick={sendVerificationEmail}
+            loading={loading}
+          >
+            {loading ? 'Sending Verification Code' : 'Send Verification Code'}
+          </Button>
           <br />
-          <Button className="pay" onClick={handleBankAccountSubmit}>Pay</Button>
+
+          {errors.verificationCode && <span className="error-message">{errors.verificationCode}</span>}
+          <br />
+
+          <Button className="pay" onClick={handleBankAccountSubmit}>
+            Pay
+          </Button>
         </div>
       )}
+      <Modal
+        title="Bank Account Details"
+        visible={downloadModalVisible}
+        onCancel={() => {
+          setDownloadModalVisible(false);
+          navigateToUserPage();
+        }}
+        footer={[
+          <Button key="back" onClick={() => setDownloadModalVisible(false)}>
+            Close
+          </Button>,
+          <Button key="picture" type="primary" onClick={() => handleDownload("picture")}>
+            Download as Picture
+          </Button>,
+          <Button key="pdf" type="primary" onClick={() => handleDownload("pdf")}>
+            Download as PDF
+          </Button>
+        ]}
+      >
+
+
+        {userbill && (
+          <div className="payment-details">
+            <h2>Payment Information:</h2>
+            <p>TransactionNo: {payments.TransactionNo}</p>
+            <p>Payment Method: Credit card</p>
+            <p>
+              Description: {payments.paymentDescription}
+            </p>
+            <p>
+              Reference Number:  {payments.ReferenceNo}
+            </p>
+            <p>
+              Total Amount:  {payments.amount}
+            </p>
+            <p>
+              Payment Date:  {payments.paymentDate}
+            </p>
+            <p>
+              Payer:  {payments.payerId}
+            </p>
+            <p> </p>
+            <p>
+              Payment Date:  {payments.paymentDate}
+            </p>
+          </div>
+        )}
+      </Modal>
     </div>
   );
 };
