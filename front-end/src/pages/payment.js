7335d102134cddacb567285587558d3dc6a450ba
beza_sebsibe <88824956+bezawitsebsibe@users.noreply.github.com>
2023-10-08 19:56:50 +0100

    Update payment.js

--- front-end/src/pages/payment.js
+++ front-end/src/pages/payment.js
@@ -68,7 +68,6 @@ const Payment = () => {
     };
 
     
-
     const fetchBanks = async () => {
       try {
         const response = await axios.get("http://localhost:3001/Agents"); // Replace with your actual endpoint
